b99d26d3a5c93fcaf208f5852eab1b5707a601b3
Natalie Monterrey <nikkis_4@hotmail.com>
2016-09-26 13:17:45 -0700

    Cleaned up the pins scaffold

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  
+ 
   def index
     @pins = Pin.all
   end
@@ -9,7 +9,8 @@ class PinsController < ApplicationController
   
   def show
   end
- 
+
+  
   def new
     @pin = Pin.new
   end
@@ -22,39 +23,38 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
 
+    
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.' 
     else
-      render action: 'new'
+      render action: 'new' 
     end
   end
 
   
-  
   def update
+    
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.' 
-        
+       redirect_to @pin, notice: 'Pin was successfully updated.' 
     else
-      render action: 'edit' 
+       render action: 'edit'
     end
   end
-  
 
   
   def destroy
     @pin.destroy
     redirect_to pins_url, notice: 'Pin was successfully destroyed.' 
-  end
   
+  end
 
   private
-   
+    
     def set_pin
       @pin = Pin.find(params[:id])
     end
 
-    
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
     end
